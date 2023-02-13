c3f487101ed7be8f4766fb74412486b45d6b35e0
johnxadams <johnxadams93@gmail.com>
2023-02-13 18:37:26 +0100

    styling lang=scss added- and working with module

---
+++ src/components/navbar.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <div v-bind:class="$style.navbarContainer">
+    <h1>navbar</h1>
+  </div>
+</template>
+
+<style module lang="scss">
+.navbarContainer {
+  background-color: tomato;
+}
+</style>
