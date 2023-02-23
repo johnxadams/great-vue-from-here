5441ec75184e2424f38c4145ba272151e6d23b52
johnxadams <johnxadams93@gmail.com>
2023-02-23 22:06:14 +0100

    navbar styles moves to scss

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -18,42 +18,5 @@
 </template>
 
 <style module lang="scss">
-@import "../scss/abstract/abstract-files.scss";
-@mixin flex-jc-ai-dir($jc, $ai, $dir) {
-  display: flex;
-  justify-content: $jc;
-  align-items: $ai;
-  flex-direction: $dir;
-}
-
-.navbarContainer {
-  border-bottom: 3px solid black;
-  ul {
-    @include flex-jc-ai-dir(flex-start, center, row);
-    li {
-      font-size: 2rem; // this is only for "Projects" in navbar
-      margin: 0 1em -0.5em 0;
-      list-style: none;
-      /* border: 2px black solid; */
-      text-shadow: $mainShadow;
-    }
-    a {
-      text-decoration: none;
-      font-size: 2rem;
-      //margin-bottom: -1rem; // this margin doesnt apply to <a>"Projects"
-      font-weight: 600;
-      color: rgb(30, 109, 179);
-      transition: ease-in-out 0.15s;
-      &:hover {
-        color: #79717a;
-      }
-      &:active {
-        color: white;
-      }
-    }
-    :nth-child(3) {
-      /* margin-right: 1.7em; */
-    }
-  }
-}
+@import "../scss/layout/navbar.scss";
 </style>
